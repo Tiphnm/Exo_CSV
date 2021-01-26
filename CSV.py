463e1ae1a0c1e5209ddd071d05420a1d377698df
Tiphaine Minguet <tiphaineminguet@MacBook-Air-de-Tiphaine-2.local>
2021-01-26 21:57:46 +0100

    La création de toutes mes loops en fonction

--- CSV.py
+++ CSV.py
@@ -13,7 +13,7 @@ def read_json(file_name):
     with open(json_file) as json_data:     
         data = json.load(json_data)
 
-    pprint.pprint(data)
+    #pprint.pprint(data)
     #print(type(data))
     #print(data.keys())
 
@@ -21,41 +21,49 @@ read_json('stop_areas.json')# appeler ma fonction avec le nom de mon 'jsonfile'
 
 '''partie request'''
 
+
 URL = "https://api.sncf.com/v1/coverage/sncf/stop_areas"
 headers = {"Authorization": "0157b284-3cc3-4799-a1ab-79dc2761d274"}
-url_request = requests.get(url = URL, headers=headers)
-raw_data = json.loads(url_request.text)
+raw_data = None
 
-#print(url_request.json())
-#print(type(raw_data))
-#print(raw_data.keys())
+def read_json():#crée mon fichier json 
+    url_request = requests.get(url = URL, headers=headers)
+    with open("stop_areas_tiph.json", mode = "w") as file : 
+        json.dump(url_request.json(), file)
+        #retourne rien, juste pour sauvegarder le json
+def_read_json = read_json()
 
-areas = raw_data['stop_areas']
+def read_links(): # enregistre mes liens 
+    with open("stop_areas_tiph.json") as json_stop_areas_file: 
+        raw = json.load(json_stop_areas_file) #pas besoin de s car mon fichier est déjà convertis en text dans ma fonction précédente
+    return raw
 
-#faire une fonction qui enregistre mon fichier 
+raw_data = read_links()
 
-#MES LINKS
 
-liste_links = [] #là ou je vais les stockés pour pouvoir les transformer en CSV par la suite 
+def loop_links(): # reads and saves json
 
-my_links = raw_data['links']
-my_link = my_links[0]
-#print(my_link)
+    my_links = raw_data['links']    
 
-for loop_link in my_links: 
-    if type(loop_link) == dict: 
-        if "href" in loop_link.keys(): 
-            local_href = loop_link["href"]
-            liste_links.append(local_href)
-        else: 
-            print("Missing key in this list")
-    else: 
-        print("Unexpected format: %s", s(type(loop_link)))        
+    liste_links = [] 
+    
+    for loop_link in my_links: 
+            if type(loop_link) == dict: 
+                if "href" in loop_link.keys(): 
+                    local_href = loop_link["href"]
+                    liste_links.append(local_href)
+                else: 
+                    print("Missing key in this list")
+            else: 
+                print("Unexpected format: %s", s(type(loop_link)))  
 
-#print(len(liste_links))
-#print(liste_links)
+    #print(type(liste_links))             
+    return liste_links
+ 
+def_loop_links = loop_links()
 
 #MES IDS 
+areas = raw_data['stop_areas']
 
 def my_id(key_name) : 
     #key_name = "id"
@@ -76,8 +84,10 @@ def my_id(key_name) :
     #print(len(liste_id))
     print(liste_id)
     #print(area.keys())
+    return liste_id
 
-my_id("id")
+liste_id = my_id("id")
+print("TEST", liste_id)
 
 #MES NOMS 
 def my_name(key_name):
@@ -94,9 +104,10 @@ def my_name(key_name):
         else: 
             print("Unexpected format: %s ", s(type(loop_name)))
 
-    print(liste_names)
+    #print(liste_names)
+    return liste_names
 
-my_name('label')
+liste_names = my_name('label')
 
 #MES COORDONNEES 
 def my_coord(key_name):
@@ -114,23 +125,30 @@ def my_coord(key_name):
             print("Unexpected type")
 
     print(liste_coord)
+    return liste_coord
 
-my_coord('coord')
+liste_coord = my_coord('coord')
+
+#TRANSFORMATION EN CSV de mes names et coord 
+
+def csv_convert_info(liste1, liste2, liste3):
+    my_dict = {'ID': liste1, 'NAME': liste2, 'COORD': liste3}
+    print("This my list", liste1)
+
+    df= pd.DataFrame(my_dict)
+
+    df.to_csv('Mon_csv.csv')
+
+    return csv_convert_info
+
+def_csv_convert_info = csv_convert_info(liste_id, liste_names, liste_coord)
 
 #TRANSFORMATION EN CSV de mes liens 
 '''
 df = pd.DataFrame(liste_links)  
     
-df.to_csv('Mes_liens.csv') '''
-
-#TRANSFORMATION EN CSV de mes names et coord 
+df.to_csv('Mes_liens.csv') 
 '''
-dict = {'ID': liste_id, 'NAME': liste_names, 'COORD': liste_coord}
-
-df= pd.DataFrame(dict)
-
-df.to_csv('Mon_csv.csv')
-'''
-#faire une fonction pour chaque key 
-#faire une fonction qui prend toutes mes fonctions key pour créer un dictionnaire 
-#faire une fonction avec ces deux fonctions pour créer des fichiers CSV 
+    #faire une fonction pour chaque key 
+    #faire une fonction qui prend toutes mes fonctions key pour créer un dictionnaire 
+    #faire une fonction avec ces deux fonctions pour créer des fichiers CSV 
